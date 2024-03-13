133db9fd96b3b93e1523f545905dd20dd5fd5d17
Chir@g159 <dchirag159@gmail.com>
2024-03-13 19:20:06 +0530

    firat program

---
+++ practice_programs/add_two_numbers/add.java
@@ -0,0 +1,11 @@
+
+
+public class add {
+    public static void main(String[] args)
+    {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + (num1+num2));
+    }    
+}
