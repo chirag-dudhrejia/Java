26cc52efeed309c2313a616c34e8ead2537f0ea8
Chir@g159 <dchirag159@gmail.com>
2024-03-30 23:20:26 +0530

    square

---
+++ practice_programs/square_of_number/square.java
@@ -0,0 +1,10 @@
+
+
+public class square {
+    public static void main(String[] args)
+    {
+        int number = Integer.parseInt(args[0]);
+
+        System.out.println("Square of " + number + " is " + number*number);
+    }
+}
